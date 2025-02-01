346e633fb8416097659141f6be2022a6a7da45a4
Ralph Marvin <ralphmarvin4@gmail.com>
2025-02-01 14:49:26 +0000

    refactor routing configuration and add Cart and ProductList components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductListComponent },
+];
